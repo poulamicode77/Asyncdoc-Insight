375459e52715e52283f48918af2f279e15f812d6
Poulami Bhattacharyya <poulamibhattacharyya77@gmail.com>
2026-01-29 11:06:05 +0530

    Updating with progress states

--- frontend/src/pages/ProgressPage.jsx
+++ frontend/src/pages/ProgressPage.jsx
@@ -1,6 +1,14 @@
 function ProgressPage() {
     return(
-        <h2>Processing Status</h2>
+        <div>
+            <h2>Processing Status</h2>
+            <ul>
+                <li>Upload Received</li>
+                <li>Reading File</li>
+                <li>Processing Data</li>
+                <li>Completed</li>
+            </ul>
+        </div>
     );
 }
 export default ProgressPage;
