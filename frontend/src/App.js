f9b4a9f8cc94cfba5b894563b1dc2d608c780438
Poulami Bhattacharyya <poulamibhattacharyya77@gmail.com>
2026-01-30 12:48:49 +0530

    Add page navigation between Upload and Progress

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,18 @@
+import { useState } from 'react';
+import ProgressPage from "./pages/ProgressPage";
 import UploadPage from "./pages/UploadPage";
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [currentPage, setCurrentPage] = useState("upload");
   return (
-    <UploadPage/>
+    <>
+      {currentPage === "upload" && (
+        <UploadPage onNext ={() => setCurrentPage("progress")}/>
+      )}
+      {currentPage === "progress" && <ProgressPage/>}
+    </>
   );
 }
 
